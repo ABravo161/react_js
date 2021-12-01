dc17b48f4cd8665160185057f50213595d78c5ca
andres.bravo <andres.bravo161@gmail.com>
2021-12-01 12:26:32 -0300

    se agrega el customHook useCounter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,28 +1,22 @@
 import React, { useState } from "react";
 import "./ItemCount.scss";
+import {useCounter} from "../../hooks/useCounter"
 
 import Button from '@material-ui/core/Button';
 
 
 export const ItemCount = ({stock =10, initial =0 }) => {
-    const [cantidad, SetCantidad] = useState(initial);
+    
+    const {counter, increment, decrement} = useCounter(initial, stock, 1)
 
-    const HandleRestar = () => {
-        cantidad > initial && SetCantidad(cantidad-1);
-    }
-
-
-    const HandleSumar = () => {
-        cantidad < stock && SetCantidad(cantidad+1);
-    }
 
     return (
         <div className="ItemCount">
         <p>Nuevo Item</p>
         
-        <Button variant="outlined" onClick={HandleRestar}>-</Button>
-        <span>{cantidad}</span>
-        <Button variant="outlined" onClick={HandleSumar}>+</Button>
+        <Button variant="outlined" onClick={decrement}>-</Button>
+        <span>{counter}</span>
+        <Button variant="outlined" onClick={increment}>+</Button>
         </div>
     );
 }
---
+++ src/hooks/useCounter.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+export const useCounter = (initial = 1, stock = 10, minimo = 1) => {
+    const [counter, setCounter] = useState(initial)
+
+    const increment = () => {
+        counter < stock && setCounter(counter + 1)
+    }
+
+    const decrement = () => {
+        counter > minimo && setCounter(counter -1)
+    }
+
+    return {
+        counter,
+        increment,
+        decrement
+    }
+
+}
