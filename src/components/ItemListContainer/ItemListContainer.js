d8f41b4d1b3ca4f68f833e18b59378a9be2cabc7
andres.bravo <andres.bravo161@gmail.com>
2021-12-01 16:06:35 -0300

    se agrega mock async service

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,27 @@
+import React, { useEffect, useState } from "react"
+import { pedirDatos } from "../../helpers/pedirDatos"
 
 
 export const ItemListContainer = ({greeting}) => {
+    
+    const [items, setItems] = useState([])
+
+    console.log(items)
+
+    useEffect(()=>{
+
+        pedirDatos()
+        .then((resp) => {
+            setItems( resp )
+        })
+        .catch( (err) => {
+            console.log(err)
+        })
+        .finally(() => {
+            console.log("peticion finalizada")
+        })
+    },[])
+
     return (
         <div>
             <h2>{greeting}</h2>
---
+++ src/datos/stock.js
@@ -0,0 +1,30 @@
+export const stock = [
+    {
+        id: 1,
+        nombre: "producto 1",
+        precio: 1000,
+        imagen: "https://via.placeholder.com/200",
+        descripcion: "lorem ipsum"
+    },
+    {
+        id: 2,
+        nombre: "producto 2",
+        precio: 1000,
+        imagen: "https://via.placeholder.com/200",
+        descripcion: "lorem ipsum"
+    },
+    {
+        id: 3,
+        nombre: "producto 3",
+        precio: 1000,
+        imagen: "https://via.placeholder.com/200",
+        descripcion: "lorem ipsum"
+    },
+    {
+        id: 4,
+        nombre: "producto 4",
+        precio: 1000,
+        imagen: "https://via.placeholder.com/200",
+        descripcion: "lorem ipsum"
+    },]
+    
