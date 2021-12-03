d61cc772e0470e0f0ea17ca1a8779daccaa4a8ed
andres.bravo <andres.bravo161@gmail.com>
2021-12-03 11:41:20 -0300

    se agrega react router y se configuran los routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import NavBar from "./components/NavBar/NavBar";
 import { Footer } from "./components/Footer/Footer"
 import { ItemListContainer } from "./components/ItemList/ItemListContainer";
+import { CartView } from "./components/CartView/CartView"
 import "./App.scss"
 import { ItemDetailContainer } from "./components/ItemDetail/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-        <NavBar />
+    <BrowserRouter>
+      
+      <NavBar />
+      
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/detail" element={<ItemDetailContainer />} />
+        <Route path="/category" element={<ItemDetailContainer />} />
+        <Route path="/cartview" element={<CartView />} />
+      </Routes>
+      
+      <Footer />
 
-        <ItemDetailContainer />
-
-        <ItemListContainer />
-
-        <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
