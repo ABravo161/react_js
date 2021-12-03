2730a25d4d64e700704a7c961e92fcf16107e03c
andres.bravo <andres.bravo161@gmail.com>
2021-12-03 16:57:45 -0300

    se sincroniza el counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -4,18 +4,18 @@ import {useCounter} from "../../hooks/useCounter"
 import Button from '@material-ui/core/Button';
 
 
-export const ItemCount = ({stock =10, initial =0 }) => {
-    
-    const {counter, increment, decrement} = useCounter(initial, stock, 1)
-
+export const ItemCount = ({increment, decrement, onAdd, counter}) => {
 
     return (
         <div className="ItemCount">
-        <p>Cantidad</p>
-        
-        <Button variant="outlined" onClick={decrement}>-</Button>
-        <span>{counter}</span>
-        <Button variant="outlined" onClick={increment}>+</Button>
+            <p>Cantidad</p>
+
+            <Button variant="outlined" onClick={decrement}>-</Button>
+            <span>{counter}</span>
+            <Button variant="outlined" onClick={increment}>+</Button>
+            <div>
+                <Button variant="contained" onClick={onAdd} counter={counter}>Agregar</Button>
+            </div>
         </div>
     );
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -2,25 +2,39 @@ import { Button } from "@material-ui/core"
 import React from "react"
 import { useNavigate } from "react-router"
 import { ItemCount } from "../ItemCount/ItemCount"
+import { useCounter } from "../../hooks/useCounter"
 import "./ItemDetail.scss"
 
 
 
 export const ItemDetail = ({item})=> {
 
+    const {counter, increment, decrement} = useCounter(1, item.stock, 1)
+
     const navigate = useNavigate()
 
     const handleVolver = () => {
         navigate(-1)
     }
+
+    const handleAgregar = () => {
+        console.log("item agregado", {
+            id: item.id,
+            precio: item.price,
+            nombre: item.title,
+            contidad: counter
+        })
+    }
+
     return (
         <div className="ItemDetail">
             <img src={item.image} alt={item.title}/>
             <h3>{item.title}</h3>
             <p>Precio: ${item.price}</p>
             <p>{item.description}</p>
-            <ItemCount />
 
-            <Button onClick={handleVolver}>Volver</Button>
+            <ItemCount increment={increment} decrement={decrement} onAdd={handleAgregar} counter={counter}/>
+
+            <Button variant="contained" onClick={handleVolver}>Volver</Button>
         </div>
     )}
